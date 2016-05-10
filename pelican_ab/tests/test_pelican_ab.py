8f553150ddd21ecc9aa58518233592038a1d3227
Mr. Senko <atodorov@mrsenko.com>
2016-05-11 00:22:49 +0300

    flake8 fixes

--- pelican_ab/tests/test_pelican_ab.py
+++ pelican_ab/tests/test_pelican_ab.py
@@ -9,17 +9,15 @@ from shutil import rmtree
 from tempfile import mkdtemp
 
 from pelican import Pelican
-# from pelican.tests.support import mute
 from pelican.settings import read_settings
 
 
 CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
 OUTPUT_PATH = os.path.abspath(os.path.join(CURRENT_DIR, 'output'))
-
 INPUT_PATH = os.path.join(CURRENT_DIR, "content")
 
-class TestPelicanAB(unittest.TestCase):
 
+class TestPelicanAB(unittest.TestCase):
     def setUp(self):
         self.temp_path = mkdtemp(prefix='pelican-ab.')
         pelican_ab_path, _ = os.path.join(os.path.split(pelican_ab.__file__))
@@ -43,21 +41,5 @@ class TestPelicanAB(unittest.TestCase):
             THEN JINJA_EXTENSIONS settings are updated
         """
         added = any([issubclass(x, jinja_ab.JinjaExperimentExtension)
-            for x in self.pelican.settings['JINJA_EXTENSIONS']])
+                    for x in self.pelican.settings['JINJA_EXTENSIONS']])
         self.assertTrue(added)
-
-#    def test_add_static_paths(self):
-#        theme = self.pelican.settings['THEME_STATIC_PATHS']
-#        self.assertTrue(self.pelicanfly_static in theme)
-
-#    def test_markdown_plugin(self):
-#        sample_output = open(os.path.join(self.temp_path, 'pages', 'a-sample-page.html'), 'r')
-#        self.assertTrue('<i class="fa fa-bug"></i>' in sample_output.read())
-
-#    def test_assets_exist(self):
-#        for static_dir in ['css', 'fonts']:
-#            static_path = os.path.join(self.pelicanfly_static, static_dir)
-#            for static_file in os.listdir(static_path):
-#                in_theme = os.path.join(self.temp_path, 'theme',
-#                                        static_dir, static_file)
-#                self.assertTrue(os.path.exists(in_theme))
