07e83d313ba765311ad52d0687287d0ec666e047
Mr. Senko <atodorov@mrsenko.com>
2016-05-11 01:06:22 +0300

    Add rendering tests

--- pelican_ab/tests/content/sample-article.markdown
+++ pelican_ab/tests/content/sample-article.markdown
@@ -1,6 +1,4 @@
 Title: A sample page
+Date: 2016-05-11 00:00
 
 No one will read this
-=====================
-
-We're on a :fa-bug: hunt! Lucky I brought my :fa-compass: ...
--- pelican_ab/tests/test_pelican_ab.py
+++ pelican_ab/tests/test_pelican_ab.py
@@ -13,33 +13,60 @@ from pelican.settings import read_settings
 
 
 CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
-OUTPUT_PATH = os.path.abspath(os.path.join(CURRENT_DIR, 'output'))
 INPUT_PATH = os.path.join(CURRENT_DIR, "content")
+THEME_PATH = os.path.join(CURRENT_DIR, "themes")
 
 
 class TestPelicanAB(unittest.TestCase):
-    def setUp(self):
+    def _render(self, run=True):
         self.temp_path = mkdtemp(prefix='pelican-ab.')
-        pelican_ab_path, _ = os.path.join(os.path.split(pelican_ab.__file__))
-        self.pelican_ab_static = os.path.join(pelican_ab_path, 'static')
+#        pelican_ab_path, _ = os.path.join(os.path.split(pelican_ab.__file__))
+#        self.pelican_ab_static = os.path.join(pelican_ab_path, 'static')
         self.settings = read_settings(path=None,
                                       override={
-                                          'PATH': INPUT_PATH,
-                                          'OUTPUT_PATH': self.temp_path,
-                                          'PLUGINS': [pelican_ab]})
+                                        'PATH': INPUT_PATH,
+                                        'OUTPUT_PATH': self.temp_path,
+                                        'THEME': os.path.join(THEME_PATH,
+                                                              'simple'),
+                                        'PLUGINS': [pelican_ab]})
         self.pelican = Pelican(self.settings)
-        self.pelican.run()
-        pass
+        if run:
+            self.pelican.run()
 
     def tearDown(self):
+        if jinja_ab._ENV in os.environ:
+            del os.environ[jinja_ab._ENV]
+
         rmtree(self.temp_path)
-        pass
 
     def test_jinja_ab_extension_is_added_to_settings(self):
         """
             WHEN this plugin is configured,
             THEN JINJA_EXTENSIONS settings are updated
         """
+        self._render(False)
         added = any([issubclass(x, jinja_ab.JinjaExperimentExtension)
                     for x in self.pelican.settings['JINJA_EXTENSIONS']])
         self.assertTrue(added)
+
+    def test_render_the_control_experiment(self):
+        """
+            WHEN AB_EXPERIMENT is not configured,
+            THEN the control experiment is rendered
+        """
+        self._render()
+        sample_output = open(os.path.join(self.temp_path, 'a-sample-page.html'), 'r').read()
+        self.assertTrue('This is the control experiment' in sample_output)
+        self.assertFalse('This is v1 experiment' in sample_output)
+
+    def test_render_another_experiment(self):
+        """
+            WHEN AB_EXPERIMENT is configured,
+            THEN that particular experiment is rendered
+        """
+        os.environ[jinja_ab._ENV] = 'v1'
+        self._render()
+        sample_output = open(os.path.join(self.temp_path, 'a-sample-page.html'), 'r').read()
+        self.assertFalse('This is the control experiment' in sample_output)
+        self.assertTrue('This is v1 experiment' in sample_output)
+
