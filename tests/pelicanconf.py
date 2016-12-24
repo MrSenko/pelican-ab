83475e3629ed3c053cc438b90a303e365ed1ca46
Mr. Senko <atodorov@mrsenko.com>
2016-12-24 10:52:53 +0200

    Set DELETE_OUTPUT_DIRECTORY to True
    there is a bug in behavior when this is set to True, see
    725f10df6684c1ccc945d911b4cbee4d09b00a5f. Setting to True to see
    if the tests will discover that bug!

--- tests/pelicanconf.py
+++ tests/pelicanconf.py
@@ -46,7 +46,7 @@ RELATIVE_URLS = False
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
 CATEGORY_FEED_ATOM = 'feeds/%s.atom.xml'
 
-DELETE_OUTPUT_DIRECTORY = False
+DELETE_OUTPUT_DIRECTORY = True
 
 PLUGIN_PATHS = ['../']
 PLUGINS = ['pelican_ab']
