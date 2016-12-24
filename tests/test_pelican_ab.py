b8bff85eeca6c18fbf62cac55fd1a0d64295c43c
Mr. Senko <atodorov@mrsenko.com>
2016-12-24 11:24:11 +0200

    Test with experiment called 'c1' to kill one mutant
    -        if (experiment != jinja_ab._ENV_DEFAULT):
    +        if (experiment > jinja_ab._ENV_DEFAULT):
    
    is killed by using additional experiment values to test with.
    The default is called 'control' and we really need to test with
    values which are < 'control' and > 'control' when compared as
    strigns!

--- tests/test_pelican_ab.py
+++ tests/test_pelican_ab.py
@@ -65,10 +65,12 @@ class TestPelicanAB(unittest.TestCase):
         self.assertTrue('href="/a-sample-page.html"' in sample_output)
         self.assertTrue('href="/author/mr-senko.html"' in sample_output)
 
-    def test_render_another_experiment(self):
+    def test_render_experiment_v1(self):
         """
             WHEN AB_EXPERIMENT is configured,
             THEN that particular experiment is rendered
+
+            NOTE: for mutation testing 'c1' < 'control' < 'v1'
         """
         v1 = 'v1'
         os.environ[jinja_ab._ENV] = v1
@@ -86,4 +88,29 @@ class TestPelicanAB(unittest.TestCase):
         # both are monkey-patched by this plugin and tested here
         self.assertTrue('href="/%s/a-sample-page.html"' % v1 in sample_output)
         self.assertTrue('href="/%s/author/mr-senko.html"' % v1 in
+                        sample_output)
+
+    def test_render_experiment_c1(self):
+        """
+            WHEN AB_EXPERIMENT is configured,
+            THEN that particular experiment is rendered
+
+            NOTE: for mutation testing 'c1' < 'control' < 'v1'
+        """
+        c1 = 'c1'
+        os.environ[jinja_ab._ENV] = c1
+
+        self._render()
+        sample_output = open(os.path.join(self.temp_path, c1,
+                                          'a-sample-page.html'), 'r').read()
+        # verify correct text is rendered
+        self.assertFalse('This is the control experiment' in sample_output)
+        self.assertTrue('This is c1 experiment' in sample_output)
+        # verify URLs have been changed to point to objects from the experiment
+        # NOTE:
+        # a-simple-page.html is {{ article.url }} aka Content.url
+        # author/mr-senko.html is {{ author.url }} aka URLWrapper.url
+        # both are monkey-patched by this plugin and tested here
+        self.assertTrue('href="/%s/a-sample-page.html"' % c1 in sample_output)
+        self.assertTrue('href="/%s/author/mr-senko.html"' % c1 in
                         sample_output)
